fe9509492a31ef350bbbe4ad9c8a27f9a6c8de1c
kimkyeongmo <76855174+kimkyeongmo@users.noreply.github.com>
2023-05-16 11:33:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-x = st.slider("select a value")
-st.write(x, 'squared is ',x*x)
+df = pd.DataFrame(np.random.randn(500,2)/[50,50] + [37.76, -122.4], colums = ['lat', 'lon'])
+st.map(df)
